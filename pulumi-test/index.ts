739570447b98fc813c2c1d4886535b41ac30d166
Steven <hoangbaoduy@gmail.com>
2025-05-08 21:01:45 +0800

    update naming conventions

--- pulumi-test/index.ts
+++ pulumi-test/index.ts
@@ -16,72 +16,72 @@ const rs = (async () => {
     preventDuplicateNames: true,
   });
 
-  const rsGroup = new RsGroup(
-    'common',
-    {
-      lock: false,
-      groupRoles,
-      roleAssignments: [rsRoleDefinitions.rsGroup.getReadOnly(), rsRoleDefinitions.keyVault],
-    },
-    { dependsOn: groupRoles },
-  );
+  // const rsGroup = new RsGroup(
+  //   'common',
+  //   {
+  //     lock: false,
+  //     groupRoles,
+  //     roleAssignments: [rsRoleDefinitions.rsGroup.getReadOnly(), rsRoleDefinitions.keyVault],
+  //   },
+  //   { dependsOn: groupRoles },
+  // );
 
-  const vaultInfo = new KeyVault(
-    'vault',
-    {
-      rsGroup,
-    },
-    { dependsOn: rsGroup },
-  );
+  // const vaultInfo = new KeyVault(
+  //   'vault',
+  //   {
+  //     rsGroup,
+  //   },
+  //   { dependsOn: rsGroup },
+  // );
 
-  const userAssignedId = new UserAssignedIdentity(
-    'azure-test',
-    {
-      rsGroup,
-      vaultInfo: vaultInfo,
-      memberof: [groupRoles.readOnly],
-    },
-    { dependsOn: [rsGroup, groupRoles, vaultInfo] },
-  );
+  // const userAssignedId = new UserAssignedIdentity(
+  //   'azure-test',
+  //   {
+  //     rsGroup,
+  //     vaultInfo: vaultInfo,
+  //     memberof: [groupRoles.readOnly],
+  //   },
+  //   { dependsOn: [rsGroup, groupRoles, vaultInfo] },
+  // );
 
-  const hub = new HubVnet(
-    'hub',
-    {
-      rsGroup,
-      securityGroup: {},
-      vnet: {
-        addressPrefixes: ['192.168.1.0/25'],
-        //defaultOutboundAccess: false,
-        subnets: [
-          { subnetName: 'primary', addressPrefix: '192.168.1.0/26' },
-          { subnetName: 'secondary', addressPrefix: '192.168.1.64/26' },
-        ],
-      },
-    },
-    { dependsOn: [rsGroup, userAssignedId] },
-  );
+  // const hub = new HubVnet(
+  //   'hub',
+  //   {
+  //     rsGroup,
+  //     securityGroup: {},
+  //     vnet: {
+  //       addressPrefixes: ['192.168.1.0/25'],
+  //       //defaultOutboundAccess: false,
+  //       subnets: [
+  //         { subnetName: 'primary', addressPrefix: '192.168.1.0/26' },
+  //         { subnetName: 'secondary', addressPrefix: '192.168.1.64/26' },
+  //       ],
+  //     },
+  //   },
+  //   { dependsOn: [rsGroup, userAssignedId] },
+  // );
 
-  const spoke = new HubVnet(
-    'spoke',
-    {
-      rsGroup,
-      securityGroup: {},
-      vnet: {
-        addressPrefixes: ['192.168.1.128/25'],
-        //defaultOutboundAccess: false,
-        subnets: [
-          { subnetName: 'primary', addressPrefix: '192.168.1.128/26' },
-          { subnetName: 'secondary', addressPrefix: '192.168.1.192/26' },
-        ],
-      },
-      vnetPeering: { vnet: hub.vnet, direction: 'Bidirectional' },
-    },
-    { dependsOn: [rsGroup, userAssignedId, hub] },
-  );
+  // const spoke = new HubVnet(
+  //   'spoke',
+  //   {
+  //     rsGroup,
+  //     securityGroup: {},
+  //     vnet: {
+  //       addressPrefixes: ['192.168.1.128/25'],
+  //       //defaultOutboundAccess: false,
+  //       subnets: [
+  //         { subnetName: 'primary', addressPrefix: '192.168.1.128/26' },
+  //         { subnetName: 'secondary', addressPrefix: '192.168.1.192/26' },
+  //       ],
+  //     },
+  //     vnetPeering: { vnet: hub.vnet, direction: 'Bidirectional' },
+  //   },
+  //   { dependsOn: [rsGroup, userAssignedId, hub] },
+  // );
 
   return {
-    rsGroup: rsGroup.PickOutputs('resourceGroupName', 'location'),
-    envRole: groupRoles.PickOutputs('admin', 'contributor', 'readOnly'),
+    //rsGroup: rsGroup.PickOutputs('resourceGroupName', 'location'),
+    //envRole: groupRoles.PickOutputs('admin', 'contributor', 'readOnly'),
     //subnets: vnet.subnets,
   };
 })();

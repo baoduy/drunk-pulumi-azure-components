c4288d98f92f138784dd3c2654cb91ac312dbc5e
Steven Hoang <hoangbaoduy@gmail.com>
2025-08-04 14:30:37 +0800

    update

--- pulumi-test/index.ts
+++ pulumi-test/index.ts
@@ -13,71 +13,12 @@ const rs = (async () => {
     {
       vault: { sku: 'standard' },
       enableDefaultUAssignId: true,
-      diskEncryption: { encryptionType: 'EncryptionAtRestWithPlatformAndCustomerKeys' },
-      // logs: {
-      //   retentionInDays: 30,
-      //   storage: { enabled: true },
-      //   workspace: { enabled: false },
-      // },
+      //diskEncryption: { encryptionType: 'EncryptionAtRestWithPlatformAndCustomerKeys' },
       groupRoles,
       roleAssignments: [rsRoleDefinitions.rsGroup.getReadOnly(), rsRoleDefinitions.keyVault],
     },
     { dependsOn: groupRoles },
   );
-
-  // const ipAddress = new IpAddresses(
-  //   'ip',
-  //   {
-  //     ...rs.getOutputs(),
-  //     sku: { name: 'Standard' },
-  //     ipAddresses: [{ name: 'outbound' }],
-  //   },
-  //   { dependsOn: rs },
-  // );
-
-  // const hub = new HubVnet(
-  //   'env',
-  //   {
-  //     rsGroup,
-  //     securityGroup: {},
-  //     publicIpAddresses: [ipAddress.ipAddresses.outbound],
-  //     natGateway: { sku: 'Standard' },
-  //     vnet: {
-  //       addressPrefixes: ['192.168.1.0/24'],
-  //       defaultOutboundAccess: false,
-  //       subnets: [{ subnetName: 'aks', addressPrefix: '192.168.1.0/25' }],
-  //     },
-  //   },
-  //   { dependsOn: [rsGroup, userAssignedId] },
-  // );
-
-  // const aks = new AzKubernetes(
-  //   'aks',
-  //   {
-  //     rsGroup,
-  //     vaultInfo: vaultInfo,
-  //     sku: 'Standard',
-  //     features: { enablePrivateCluster: true },
-  //     agentPoolProfiles: [
-  //       {
-  //         name: 'defaultpool',
-  //         enableAutoScaling: true,
-  //         minCount: 1,
-  //         maxCount: 3,
-  //         mode: 'System',
-  //         type: 'VirtualMachineScaleSets',
-  //         vmSize: 'Standard_D2as_v4',
-  //         osDiskSizeGB: 128,
-  //         osType: 'Linux',
-  //         vnetSubnetID: hub.subnets.aks.id,
-  //       },
-  //     ],
-  //     network: {
-  //       outboundType: 'userDefinedRouting',
-  //     },
-  //   },
-  //   { dependsOn: [rsGroup, hub, userAssignedId, ipAddress, vaultInfo] },
-  // );
 
   return {
     ...rs.getOutputs(),

1e1599f54dba5bcc1ac1dea667c8833b1fbb97c5
Steven Hoang <drunkcoding@outlook.com>
2026-01-17 08:44:08 +0800

    fix group issues to prevent missing manual member assignment

--- src/azAd/AzRole.ts
+++ src/azAd/AzRole.ts
@@ -5,46 +5,69 @@ import { BaseComponent } from '../base/BaseComponent';
 import { getComponentResourceType } from '../base/helpers';
 import { stackInfo } from '../helpers';
 
-export interface AzRoleArgs extends Partial<Pick<azAd.GroupArgs, 'members' | 'owners' | 'preventDuplicateNames'>> {}
+export interface AzRoleArgs extends Partial<Pick<azAd.GroupArgs, 'owners' | 'preventDuplicateNames' | 'description'>> {
+  members?: pulumi.Input<string>[];
+}
 
 export class AzRole extends BaseComponent<AzRoleArgs> {
-  public readonly objectId: pulumi.Output<string>;
-  public readonly displayName: pulumi.Output<string>;
+  public readonly group: azAd.Group;
 
   constructor(name: string, args: AzRoleArgs = {}, opts?: pulumi.ComponentResourceOptions) {
-    const n = `rol-${name.toLowerCase().replace(/\s+/g, '-')}`;
-    super(getComponentResourceType('AzRole'), n, args, opts);
+    const rsName = `rol-${name.toLowerCase().replace(/\s+/g, '-')}`;
+    super(getComponentResourceType('AzRole'), rsName, args, opts);
 
-    const roleName = name.includes(stackInfo.stack)
-      ? `ROL ${name}`.toUpperCase()
-      : `ROL ${stackInfo.stack} ${name}`.toUpperCase();
-
-    const role = new azAd.Group(
-      n,
-      {
-        displayName: roleName,
-        description: roleName,
-        members: args.members,
-        owners: args.owners,
-
-        securityEnabled: true,
-        mailEnabled: false,
-        preventDuplicateNames: args.preventDuplicateNames,
-        assignableToRole: false,
-      },
-      { parent: this },
-    );
-
-    this.objectId = role.objectId;
-    this.displayName = role.displayName;
+    this.group = this.createGroup(rsName);
+    this.createMembers(this.group);
 
     this.registerOutputs();
   }
 
   public getOutputs() {
     return {
-      objectId: this.objectId,
-      displayName: this.displayName,
+      objectId: this.group.objectId,
+      displayName: this.group.displayName,
     };
+  }
+
+  private createGroup(rsName: string) {
+    const { description, owners, preventDuplicateNames } = this.args;
+
+    const roleName = this.name.includes(stackInfo.stack)
+      ? `ROL ${this.name}`.toUpperCase()
+      : `ROL ${stackInfo.stack} ${this.name}`.toUpperCase();
+
+    return new azAd.Group(
+      rsName,
+      {
+        displayName: roleName,
+        description: description ?? roleName,
+        //members: args.members,
+        owners: owners,
+        preventDuplicateNames,
+        securityEnabled: true,
+        mailEnabled: false,
+        assignableToRole: false,
+      },
+      { parent: this, ignoreChanges: ['members'] },
+    );
+  }
+
+  private createMembers(group: azAd.Group) {
+    const { members } = this.args;
+    if (!members || members.length === 0) return;
+
+    pulumi.output(members).apply((ms) =>
+      ms.map(
+        (m) =>
+          new azAd.GroupMember(
+            `${this.name}-mb-${m}`,
+            {
+              memberObjectId: m,
+              groupObjectId: group.objectId,
+            },
+            { dependsOn: group, parent: this, deletedWith: group, deleteBeforeReplace: true },
+          ),
+      ),
+    );
   }
 }
